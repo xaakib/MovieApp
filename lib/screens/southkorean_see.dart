58322dae89c87846069e3ffe7419ecc574974af7
Xaakib <xaakibx@gmail.com>
2020-08-23 14:14:45 +0600

    changed

--- lib/screens/southkorean_see.dart
+++ lib/screens/southkorean_see.dart
@@ -4,6 +4,7 @@ import 'package:newapp/screens/southkorean_list.dart';
 import 'package:newapp/screens/Data/data.dart';
 
 class SouthkoreanSee extends StatelessWidget {
+  final 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
